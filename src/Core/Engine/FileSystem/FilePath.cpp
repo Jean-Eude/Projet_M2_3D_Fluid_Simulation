472a6967e286c6016775047aa64082c04a77b77b
Jean-Eude <jeaneude149@gmail.com>
2024-09-22 21:58:09 +0200

    Base de code

--- src/Core/Engine/FileSystem/FilePath.cpp
+++ src/Core/Engine/FileSystem/FilePath.cpp
@@ -2,6 +2,5 @@
 
 std::string FilePath::getFilePath(const std::string& filepath) {
     std::string basePath = std::filesystem::current_path().parent_path().string();
-    std::cout << basePath + filepath << std::endl;
     return basePath + filepath;
 }
